347428414d132aec00a43c13852c58d8b3da9520
divnaoki <naokiimai1@gmail.com>
2022-07-05 23:01:28 +0900

    login,logout実装

--- core/views.py
+++ core/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from .models import Profile
+from django.contrib.auth.decorators import login_required
 # error message output module
 from django.contrib import messages 
 from django.http import HttpResponse
 # Create your views here.
+@login_required(login_url='signin')
 def index(request):
   return render(request, 'index.html')
 
@@ -27,15 +29,36 @@ def signup(request):
         user = User.objects.create_user(username=username, email=email, password=password)
         user.save()
         # Log User in and redirect to settings page
-
+        user_login = auth.authenticate(username=username, password=password)
+        auth.login(request, user_login)
         # create a Profile object for the new user
         user_model = User.objects.get(username=username)
         new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
         new_profile.save()
-        return redirect('signup')
+        return redirect('settings')
     else:
       messages.info(request, 'Password Not Matching')
       return redirect('signup')
   else:
     return render(request, 'signup.html')
     
+def signin(request):
+  if request.method == "POST":
+    username = request.POST["username"]
+    password = request.POST["password"]
+
+    user = auth.authenticate(username=username, password=password)
+
+    if user is not None:
+      auth.login(request, user)
+      return redirect('/')
+    else:
+      messages.info(request, "Credentials Invalid")
+      return redirect('signin')
+  else:
+    return render(request, "signin.html")
+
+@login_required(login_url='signin')
+def logout(request):
+  auth.logout(request)
+  return redirect('signin')
